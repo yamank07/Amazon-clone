a4cb0d0b22b9707c8ff12f8adbaca3cd24f11c2f
yamank <yamkumarsgfi.yk@gmail.com>
2025-03-21 09:27:49 +0530

    Dynamic landing page of Amazon-clone is completed using HTML, tailwindcss and JS.

--- dynamic-js/src/script.js
+++ dynamic-js/src/script.js
@@ -17,6 +17,28 @@ fetch("./src/components/footer.html")
     foot.innerHTML = data2;
   });
 
+// Click address section
+document.querySelector(".addr").addEventListener("click", (e) => {
+  e.stopPropagation();
+  overlayDiv.classList.remove("hidden");
+  overlayDiv.classList.add("block");
+  document.querySelector(".addr-alert").classList.remove("hidden");
+});
+
+// Close address alert by click close btn
+document.querySelector(".close-addr").addEventListener("click", (e) => {
+  e.stopPropagation();
+  overlayDiv.classList.add("hidden");
+  overlayDiv.classList.remove("block");
+  document.querySelector(".addr-alert").classList.add("hidden");
+});
+document.querySelector(".overlay").addEventListener("click", (e) => {
+  e.stopPropagation();
+  overlayDiv.classList.add("hidden");
+  overlayDiv.classList.remove("block");
+  document.querySelector(".addr-alert").classList.add("hidden");
+});
+
 // Change select in search section
 const selectDrop = document.querySelector("#opt");
 const searchSection = document.querySelector(".search");
@@ -42,7 +64,7 @@ document.querySelector("#search-ip").addEventListener("click", (e) => {
 // Click & Scroll search section
 events.forEach((eachEvent) => {
   if (eachEvent === "click") {
-    overlayDiv.addEventListener(eachEvent, (e) => {
+    overlayDiv.addEventListener("click", (e) => {
       e.stopPropagation();
       overlayDiv.classList.remove("block");
       overlayDiv.classList.add("hidden");
@@ -119,20 +141,21 @@ events.forEach((eachEvent) => {
 // Click All section
 document.querySelector(".all-container").addEventListener("click", (e) => {
   e.stopPropagation();
-  document.querySelector(".all-drop-container").classList.remove("hidden");
+  document.querySelector(".all-drop-container").style.transform =
+    "translateX(120%)";
   document.querySelector("BODY").classList.add("overflow-y-hidden");
-  // document.querySelector("#navbar1").classList.remove("z-");
   overlayDiv.classList.remove("hidden");
   overlayDiv.classList.add("block");
   overlayDiv.classList.add("top-0");
-  overlayDiv.classList.add("bg-black/70");
+  overlayDiv.classList.add("bg-black/80");
   overlayDiv.classList.add("z-3");
 });
 
 // Close All Section
 document.querySelector(".close-btn").addEventListener("click", (e) => {
   e.stopPropagation();
-  document.querySelector(".all-drop-container").classList.add("hidden");
+  document.querySelector(".all-drop-container").style.transform =
+    "translateX(0%)";
   document.querySelector("BODY").classList.remove("overflow-y-hidden");
   overlayDiv.classList.remove("block");
   overlayDiv.classList.add("hidden");
@@ -142,7 +165,8 @@ document.querySelector(".close-btn").addEventListener("click", (e) => {
 });
 document.querySelector(".overlay").addEventListener("click", (e) => {
   e.stopPropagation();
-  document.querySelector(".all-drop-container").classList.add("hidden");
+  document.querySelector(".all-drop-container").style.transform =
+    "translateX(0%)";
   document.querySelector("BODY").classList.remove("overflow-y-hidden");
   overlayDiv.classList.remove("block");
   overlayDiv.classList.add("hidden");
@@ -204,3 +228,226 @@ events.forEach((eachEvent) => {
     });
   }
 });
+
+// Click to change hero images
+const heroImgs = document.querySelectorAll(".hero-imgs");
+let heroSlideStart = 0;
+let heroSlideEnd = (heroImgs.length - 1) * 100;
+document.querySelector(".hero-btn-r").addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (heroSlideStart != -heroSlideEnd) {
+    heroSlideStart -= 100;
+    // sessionStorage.setItem("slideStart", heroSlideStart);
+  }
+  // else if (heroSlideStart === heroSlideEnd) {
+  //   sessionStorage.setItem("slideStart", 500);
+  // }
+  heroImgs.forEach((eachImg) => {
+    // eachImg.classList.add(`-translate-x-[${heroSlideStart}%]`);
+    eachImg.style.transform = `translateX(${heroSlideStart}%)`;
+  });
+});
+document.querySelector(".hero-btn-l").addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (heroSlideStart < 0) {
+    heroSlideStart += 100;
+  }
+  heroImgs.forEach((eachImg) => {
+    // eachImg.classList.remove(`-translate-x-[${heroSlideEnd}]`);
+    eachImg.style.transform = `translateX(${heroSlideStart}%)`;
+  });
+});
+
+// Click to slide row3 items
+const slideRow3 = document.querySelectorAll(".slide-row3-box");
+let slideRowStart3 = 0;
+let slideRowEnd3 = (slideRow3.length - 1) * 100;
+document.querySelector("#slide-arrow-R3").addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (slideRowStart3 != -slideRowEnd3) {
+    slideRowStart3 -= 100;
+  }
+  slideRow3.forEach((element) => {
+    element.style.transform = `translateX(${slideRowStart3}%)`;
+  });
+});
+document.querySelector("#slide-arrow-L3").addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (slideRowStart3 < 0) {
+    slideRowStart3 += 100;
+  }
+  slideRow3.forEach((element) => {
+    element.style.transform = `translateX(${slideRowStart3}%)`;
+  });
+});
+
+// Click to slide row4 items
+const slideRow4 = document.querySelectorAll(".slide-row4-box");
+let slideRowStart4 = 0;
+let slideRowEnd4 = (slideRow4.length - 1) * 100;
+document.querySelector("#slide-arrow-R4").addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (slideRowStart4 != -slideRowEnd4) {
+    slideRowStart4 -= 100;
+  }
+  slideRow4.forEach((element) => {
+    element.style.transform = `translateX(${slideRowStart4}%)`;
+  });
+});
+document.querySelector("#slide-arrow-L4").addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (slideRowStart4 < 0) {
+    slideRowStart4 += 100;
+  }
+  slideRow4.forEach((element) => {
+    element.style.transform = `translateX(${slideRowStart4}%)`;
+  });
+});
+
+// Function to select the BOX4 ROW elements
+const totalIndex = [];
+for (let i = 0; i < 4; i++) {
+  totalIndex.push(i);
+}
+const selectFunc = (arr1, arr2, arr3) => {
+  for (let i = 0; i < arr1.length; i++) {
+    arr1[i].addEventListener("click", (e) => {
+      e.stopPropagation();
+      arr1[i].classList.remove("outline-transparent");
+      arr1[i].classList.add("outline-blue-700");
+      // console.log("removed hidden");5
+      arr2[i].classList.remove("hidden");
+      const remainIndex = arr3.filter((value) => {
+        return value != i;
+      });
+      remainIndex.forEach((ind) => {
+        arr1[ind].classList.add("outline-transparent");
+        arr1[ind].classList.add("outline-blue-700");
+        // console.log("added hidden");
+        arr2[ind].classList.add("hidden");
+      });
+    });
+  }
+};
+
+// Click in Row5
+const miniBoxes51 = document.querySelectorAll(".mini-box51");
+const boxes51 = document.querySelectorAll(".box51");
+const miniBoxes52 = document.querySelectorAll(".mini-box52");
+const boxes52 = document.querySelectorAll(".box52");
+const miniBoxes53 = document.querySelectorAll(".mini-box53");
+const boxes53 = document.querySelectorAll(".box53");
+selectFunc(miniBoxes51, boxes51, totalIndex);
+selectFunc(miniBoxes52, boxes52, totalIndex);
+selectFunc(miniBoxes53, boxes53, totalIndex);
+
+// Click to slide row6 items
+const slideRow6 = document.querySelectorAll(".slide-row6-box");
+let slideRowStart6 = 0;
+let slideRowEnd6 = slideRow6.length * 45;
+document.querySelector("#slide-arrow-R6").addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (slideRowStart6 != -slideRowEnd6) {
+    slideRowStart6 -= 45;
+  }
+  slideRow6.forEach((element) => {
+    element.style.transform = `translateX(${slideRowStart6}%)`;
+  });
+});
+document.querySelector("#slide-arrow-L6").addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (slideRowStart6 < 0) {
+    slideRowStart6 += 45;
+  }
+  slideRow6.forEach((element) => {
+    element.style.transform = `translateX(${slideRowStart6}%)`;
+  });
+});
+
+// Click to slide row7 items
+const slideRow7 = document.querySelectorAll(".slide-row7-box");
+let slideRowStart7 = 0;
+let slideRowEnd7 = (slideRow7.length - 1) * 100;
+document.querySelector("#slide-arrow-R7").addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (slideRowStart7 != -slideRowEnd7) {
+    slideRowStart7 -= 100;
+  }
+  slideRow7.forEach((element) => {
+    element.style.transform = `translateX(${slideRowStart7}%)`;
+  });
+});
+document.querySelector("#slide-arrow-L7").addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (slideRowStart7 < 0) {
+    slideRowStart7 += 100;
+  }
+  slideRow7.forEach((element) => {
+    element.style.transform = `translateX(${slideRowStart7}%)`;
+  });
+});
+
+// Click in Row8
+const miniBoxes81 = document.querySelectorAll(".mini-box81");
+const boxes81 = document.querySelectorAll(".box81");
+const miniBoxes83 = document.querySelectorAll(".mini-box83");
+const boxes83 = document.querySelectorAll(".box83");
+const miniBoxes84 = document.querySelectorAll(".mini-box84");
+const boxes84 = document.querySelectorAll(".box84");
+selectFunc(miniBoxes81, boxes81, totalIndex);
+selectFunc(miniBoxes83, boxes83, totalIndex);
+selectFunc(miniBoxes84, boxes84, totalIndex);
+
+// Click to slide row9 items
+const slideRow9 = document.querySelectorAll(".slide-row9-box");
+let slideRowStart9 = 0;
+let slideRowEnd9 = (slideRow9.length - 1) * 100;
+document.querySelector("#slide-arrow-R9").addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (slideRowStart9 != -slideRowEnd9) {
+    slideRowStart9 -= 100;
+  }
+  slideRow9.forEach((element) => {
+    element.style.transform = `translateX(${slideRowStart9}%)`;
+  });
+});
+document.querySelector("#slide-arrow-L9").addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (slideRowStart9 < 0) {
+    slideRowStart9 += 100;
+  }
+  slideRow9.forEach((element) => {
+    element.style.transform = `translateX(${slideRowStart9}%)`;
+  });
+});
+
+// Click to slide row11 items
+const slideRow11 = document.querySelectorAll(".slide-row11-box");
+let slideRowStart11 = 0;
+let slideRowEnd11 = (slideRow11.length + 1) * 29;
+document.querySelector("#slide-arrow-R11").addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (slideRowStart11 != -slideRowEnd11) {
+    slideRowStart11 -= 29;
+  }
+  slideRow11.forEach((element) => {
+    element.style.transform = `translateX(${slideRowStart11}%)`;
+  });
+});
+document.querySelector("#slide-arrow-L11").addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (slideRowStart11 < 0) {
+    slideRowStart11 += 29;
+  }
+  slideRow11.forEach((element) => {
+    element.style.transform = `translateX(${slideRowStart11}%)`;
+  });
+});
+
+// fetch("https://dummyjson.com/products/categories")
+//   .then((res) => res.json())
+//   .then(console.log);
+
+// fetch("https://dummyjson.com/products")
+//   .then((res) => res.json())
+//   .then(console.log);
